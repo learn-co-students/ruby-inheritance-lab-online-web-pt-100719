a8eef4eb1723ca33c18fa67662fe30a4d0881823
Tariq Bailey <tariqbailey627@gmail.com>
2019-11-13 00:57:43 +0000

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -1,3 +1,14 @@
-class Student 
+class Student  < User
 
+def initialize(knowledge = [])
+  @knowledge = knowledge
+end
+
+def learn(facts)
+  @knowledge << facts
+end
+
+def knowledge
+  @knowledge
+end
 end
